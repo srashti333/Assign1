97ef84e572aee3a652525d6ff0d528695a0b3fcd
srashti333 <91120117+srashti333@users.noreply.github.com>
2021-11-30 17:09:20 +0530

    Initial commit

---
+++ src/one/Wra.java
@@ -0,0 +1,22 @@
+//Wrapper class assignment 1
+package one;
+
+public class Wra {
+    public static void main(String args[]){
+        System.out.println("MIN Int = "+ Integer.MIN_VALUE);
+        System.out.println("MAX Int = "+ Integer.MAX_VALUE);
+        System.out.println("MIN Byte = "+ Byte.MIN_VALUE );
+        System.out.println("MAX Byte = "+ Byte.MAX_VALUE);
+        System.out.println("MIN Short = "+ Short.MIN_VALUE);
+        System.out.println("MAX Short = "+ Short.MAX_VALUE);
+        System.out.println("MIN Long = "+ Long.MIN_VALUE);
+        System.out.println("MAX Long = "+ Long.MAX_VALUE);
+        System.out.println("MIN Float = "+ Float.MIN_VALUE);
+        System.out.println("MAX Float = "+ Float.MAX_VALUE);
+        System.out.println("MIN Double = "+ Double.MIN_VALUE);
+        System.out.println("MAX Double = "+ Double.MAX_VALUE);
+    }
+
+
+
+}
